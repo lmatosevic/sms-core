fe050a336b0d685476b9cde46ca39366b2f88dc6
lujo5 <lukamatosevic5@gmail.com>
2018-01-28 20:09:25 +0100

    Created first version of TCP server with program arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::env;
+use std::i16;
+use std::string::String;
+use std::str::FromStr;
+
+extern crate getopts;
+
+use getopts::Options;
+
+pub mod server;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} -p PORT -i INTERFACE", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn parse_args() -> (Option<String>, Option<i16>) {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("i", "interface", "set server interface", "INTERFACE");
+    opts.optopt("p", "port", "set server port", "PORT");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return (None, None);
+    }
+    let interface = matches.opt_str("i");
+    let port = matches.opt_str("p");
+
+    if port == None || interface == None {
+        print_usage(&program, opts);
+        return (None, None);
+    }
+
+    let port_final = match i16::from_str(port.unwrap().as_str().as_ref()) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Arguments error: {}", e);
+            print_usage(&program, opts);
+            return (None, None);
+        }
+    };
+    return (interface, Option::from(port_final));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (interface, port) = parse_args();
+    if interface == None || port == None {
+        return;
+    }
+
+    let server = server::tcp_server::TCPServer::new(interface.unwrap(), port.unwrap());
+    let server_thread = server.start();
+    server_thread.join().expect("Joining server thread failed");
 }
---
+++ src/server/tcp_server.rs
@@ -0,0 +1,53 @@
+use std::io::{Read, Write, BufReader, BufWriter};
+use std::net::{TcpListener, TcpStream};
+use std::vec::Vec;
+use std::string::String;
+use std::str;
+use std::thread;
+use std::thread::JoinHandle;
+
+pub struct TCPServer {
+    interface: String,
+    port: i16,
+}
+
+impl TCPServer {
+    pub fn new(interface: String, port: i16) -> TCPServer {
+        TCPServer { interface, port }
+    }
+
+    pub fn start(&self) -> JoinHandle<()> {
+        let address = format!("{}:{}", self.interface, self.port.to_string());
+        let listener = TcpListener::bind(address).expect("Unable to bind");
+        println!("Server ready to accept TCP connections");
+
+        let server_thread = thread::spawn(move || {
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        thread::spawn(move || {
+                            TCPServer::handle_client(stream);
+                        });
+                    }
+                    Err(e) => panic!("Connection failed: {}", e)
+                }
+            }
+        });
+        return server_thread;
+    }
+
+    fn handle_client(stream: TcpStream) {
+        let mut writer = BufWriter::new(&stream);
+        writer.write_all(b"Hello user\r\n").expect("Error writing");
+        writer.flush().expect("Could not flush");
+
+        let mut reader = BufReader::new(&stream);
+        let mut buffer = Vec::new();
+        reader.read_to_end(&mut buffer).expect("Error reading");
+        let response = match str::from_utf8(buffer.as_ref()) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {error}", error = e),
+        };
+        println!("Received: {}", response);
+    }
+}
